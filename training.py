9a562734607b2387f805f3ab91817bfea6906dcf
Anton <apalme17@student.aau.dk>
2019-10-02 14:26:25 +0200

    fixes

--- training.py
+++ training.py
@@ -49,7 +49,7 @@ count = 1
 number_of_images = len(all_img_paths)
 for img_path in all_img_paths:
     img_path = img_path.replace("\\", "/")
-    print(str(count) + "/" + str(number_of_images) + ": " + img_path)
+    print(str(count) + "/" + str(number_of_images))
 
     count += 1
 

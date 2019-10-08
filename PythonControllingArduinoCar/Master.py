5a68fed1120afb692a46465e00e663f3e686ca6a
faje17 <34160668+faje17@users.noreply.github.com>
2019-10-08 14:37:15 +0200

    Controlling arduino car with python
    arduino og python kode der gør det muligt at kontrollerer hastigheden af billen ved at skrive 1, 2, 3 eller 4 i python shellen.

---
+++ PythonControllingArduinoCar/Master.py
@@ -0,0 +1,34 @@
+import serial
+
+def high_speed():
+    arduinoData.write(b'1')
+
+
+def medium_speed():
+    arduinoData.write(b'2')
+
+
+def low_speed():
+    arduinoData.write(b'3')
+
+
+def stop_motor():
+    arduinoData.write(b'4')
+
+
+arduinoData = serial.Serial('com11', 115200)
+
+x = 90
+
+while x != 9:    
+    x = input()
+
+    if x == '1':
+        high_speed()
+    elif x == '2':
+        medium_speed()
+    elif x == '3':
+        low_speed()
+    elif x == '4':
+        stop_motor()
+    

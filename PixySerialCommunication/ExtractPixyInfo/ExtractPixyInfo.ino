8a794676c2f3cb2af2345706a48887fcae6881eb
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-08 15:20:02 +0200

    Added the Arduino file in that is used to write to the serial

---
+++ PixySerialCommunication/ExtractPixyInfo/ExtractPixyInfo.ino
@@ -0,0 +1,41 @@
+#include <Pixy2.h>
+
+// This is the main Pixy object 
+Pixy2 pixy;
+
+void setup()
+{
+  Serial.begin(115200);  
+  pixy.init();
+}
+
+void loop()
+{ 
+  int i; 
+  int ageNeeded = 100;
+  // grab blocks!
+  pixy.ccc.getBlocks();
+  
+  // If there are detect blocks, print them!
+  if (pixy.ccc.numBlocks)
+  {
+    //Serial.print("Detected ");
+    //Serial.println(pixy.ccc.numBlocks);
+    for (i=0; i<pixy.ccc.numBlocks; i++)
+    {
+      if(pixy.ccc.blocks[i].m_age > ageNeeded) { //We first send the data to the serial when the block has been seen more than 100 frames. 
+        //Serial.print(i);
+        Serial.print("x:");
+        Serial.print(pixy.ccc.blocks[i].m_x);
+        Serial.print(", y:");
+        Serial.print(pixy.ccc.blocks[i].m_y);
+        Serial.print(", w:");
+        Serial.print(pixy.ccc.blocks[i].m_width);
+        Serial.print(", h:");
+        Serial.print(pixy.ccc.blocks[i].m_height);
+        Serial.print(",\n");
+      }
+      //pixy.ccc.blocks[i].print();
+    }
+  }  
+}

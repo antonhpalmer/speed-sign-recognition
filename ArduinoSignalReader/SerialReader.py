5788235f6d72dfea204f044ed22fd5a02a796fd6
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-08 15:00:35 +0200

    Changed name of a function

--- ArduinoSignalReader/SerialReader.py
+++ ArduinoSignalReader/SerialReader.py
@@ -3,7 +3,7 @@ import os
 from subprocess import Popen
 import serial
 import re
-
+from PixyCommunication.PixySnapper import PixySnapper
 
 def get_string_from_pattern(pattern, data_string):
     info = re.search(pattern, data_string)
@@ -14,6 +14,8 @@ def get_string_from_pattern(pattern, data_string):
 
 
 def get_serial_data(serial_number):
+    snapper = PixySnapper()
+
     ser = serial.Serial('COM4', serial_number)
 
     # We have an error msg on the first line which we ignore.
@@ -23,13 +25,14 @@ def get_serial_data(serial_number):
         data = ser.readline()
         data_string = str(data)
 
+        # We just give a regular expression equal to what we are looking for in the data string.
         x = get_string_from_pattern('x:(.+?),', data_string)
         y = get_string_from_pattern('y:(.+?),', data_string)
         w = get_string_from_pattern('w:(.+?),', data_string)
         h = get_string_from_pattern('h:(.+?),', data_string)
 
-        # We just give a regular expression equal to what we are looking for in the data string.
         print("x:", x, "y:", y, "w:", w, "h:", h)
+        snapper.get_cropped_image(x, y, w, h)
 
 
 get_serial_data(115200)

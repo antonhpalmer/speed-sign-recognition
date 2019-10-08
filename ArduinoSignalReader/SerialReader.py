92861fb52f4bf11ba38b312db2b9ab290cd28811
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-08 14:29:10 +0200

    Made a function for the serial reader

--- ArduinoSignalReader/SerialReader.py
+++ ArduinoSignalReader/SerialReader.py
@@ -4,8 +4,6 @@ from subprocess import Popen
 import serial
 import re
 
-# Timeout means that if it has not read a full line in X seconds it sends what it has read until know
-ser = serial.Serial('COM4', 115200)
 
 def get_string(pattern, data_string):
     info = re.search(pattern, data_string)
@@ -14,15 +12,20 @@ def get_string(pattern, data_string):
     else:
         return ""
 
-while True:
-    data = ser.readline()
-    data_string = str(data)
 
-    # We just give a regular expression equal to what we are looking for in the data string.
-    x = get_string('x:(.+?),', data_string)
-    y = get_string('y:(.+?),', data_string)
-    w = get_string('w:(.+?),', data_string)
-    h = get_string('h:(.+?),', data_string)
-    print("x:", x, "y:", y, "w:", w, "h:", h)
-    # print(data)
+def get_serial_data(serial_number):
+    ser = serial.Serial('COM4', serial_number)
 
+    while True:
+        data = ser.readline()
+        data_string = str(data)
+
+        # We just give a regular expression equal to what we are looking for in the data string.
+        x = get_string('x:(.+?),', data_string)
+        y = get_string('y:(.+?),', data_string)
+        w = get_string('w:(.+?),', data_string)
+        h = get_string('h:(.+?),', data_string)
+        print("x:", x, "y:", y, "w:", w, "h:", h)
+
+
+get_serial_data(115200)

e4d1bf6ada266f24d81057cacf823237fa028b20
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-08 14:37:48 +0200

    Changed name of a function

--- ArduinoSignalReader/SerialReader.py
+++ ArduinoSignalReader/SerialReader.py
@@ -5,7 +5,7 @@ import serial
 import re
 
 
-def get_string(pattern, data_string):
+def get_string_from_pattern(pattern, data_string):
     info = re.search(pattern, data_string)
     if info:
         return info.group(1)
@@ -16,15 +16,19 @@ def get_string(pattern, data_string):
 def get_serial_data(serial_number):
     ser = serial.Serial('COM4', serial_number)
 
+    # We have an error msg on the first line which we ignore.
+    ser.readline()
+
     while True:
         data = ser.readline()
         data_string = str(data)
 
+        x = get_string_from_pattern('x:(.+?),', data_string)
+        y = get_string_from_pattern('y:(.+?),', data_string)
+        w = get_string_from_pattern('w:(.+?),', data_string)
+        h = get_string_from_pattern('h:(.+?),', data_string)
+
         # We just give a regular expression equal to what we are looking for in the data string.
-        x = get_string('x:(.+?),', data_string)
-        y = get_string('y:(.+?),', data_string)
-        w = get_string('w:(.+?),', data_string)
-        h = get_string('h:(.+?),', data_string)
         print("x:", x, "y:", y, "w:", w, "h:", h)
 
 

ba3aee8f0cc341d73d03b01a691e1e420fd2fde2
Anton <apalme17@student.aau.dk>
2019-10-13 19:17:33 +0200

    fix

--- main.py
+++ main.py
@@ -8,26 +8,23 @@ from classification.training.training import ModelTrainer
 
 
 class Main:
-    # csv_path = 'results/cnn_model_test_results.csv'
-    # model = models.get_cnn_model()
-    # with open(csv_path, mode='w') as results_file:
-    #     writer = csv.writer(results_file, delimiter=';')
-    #     writer.writerow(['Epoch', 'Accuracy'])
-    #
-    # for epoch in range(1, 30):
-    #     h5_filename = 'classification/models/cnn_model/cnn_model' + str(epoch) + '.h5'
-    #     trainer = ModelTrainer('GTSRB/Final_Training/Images/', 0.1)
-    #     trainer.train_model(model, h5_filename, 1)
-    #
-    #     tester = ModelTester(model, 'GTSRB/Final_Test/Images/', 'GTSRB/GT-final_test.csv')
-    #     accuracy = tester.test_using_dataset()
-    #
-    #     with open(csv_path, mode='a') as results_file:
-    #         writer = csv.writer(results_file, delimiter=';')
-    #         writer.writerow([str(epoch), str(accuracy)])
-    #
-    #     model = load_model(h5_filename)
-
+    csv_path = 'results/cnn_model_test_results.csv'
     model = models.get_cnn_model()
-    trainer = ModelTrainer('GTSRB/Final_Training/Images/', 0.01)
-    trainer.train_model(model, 'model.h5', 10)
+    with open(csv_path, mode='w') as results_file:
+        writer = csv.writer(results_file, delimiter=';')
+        writer.writerow(['Epoch', 'Accuracy'])
+
+    for epoch in range(1, 30):
+        h5_filename = 'classification/models/cnn_model/cnn_model' + str(epoch) + '.h5'
+        trainer = ModelTrainer('GTSRB/Final_Training/Images/', 0.1)
+        trainer.train_model(model, h5_filename, 1)
+
+        tester = ModelTester(model, 'GTSRB/Final_Test/Images/', 'GTSRB/GT-final_test.csv')
+        accuracy = tester.test_using_dataset()
+
+        with open(csv_path, mode='a') as results_file:
+            writer = csv.writer(results_file, delimiter=';')
+            writer.writerow([str(epoch), str(accuracy)])
+
+        # model = load_model(h5_filename)
+

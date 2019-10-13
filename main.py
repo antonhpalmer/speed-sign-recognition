35755d56f423a8c0d3fdada71876a0bfcbcbddb3
Anton <apalme17@student.aau.dk>
2019-10-13 15:45:57 +0200

    fixes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import csv
 
-from keras.models import load_model
-
 from classification.models import models
 from classification.test.test import ModelTester
 from classification.training.training import ModelTrainer
@@ -15,15 +13,14 @@ class Main:
         writer.writerow(['Epoch', 'Accuracy'])
 
     for epoch in range(1, 30):
-        h5_filename = 'models/cnn_model' + str(epoch) + '.h5'
+        h5_filename = 'cnn_model' + str(epoch) + '.h5'
         trainer = ModelTrainer('GTSRB/Final_Training/Images/', 0.1)
         trainer.train_model(model, h5_filename, 1)
 
-        tester = ModelTester(h5_filename, 'GTSRB/Final_Test/Images/', 'GTSRB/GT-final_test.csv')
+        tester = ModelTester(model, 'GTSRB/Final_Test/Images/', 'GTSRB/GT-final_test.csv')
         accuracy = tester.test_using_dataset()
 
         with open(csv_path, mode='a') as results_file:
             writer = csv.writer(results_file, delimiter=';')
             writer.writerow([str(epoch), str(accuracy)])
 
-        model = load_model(h5_filename)

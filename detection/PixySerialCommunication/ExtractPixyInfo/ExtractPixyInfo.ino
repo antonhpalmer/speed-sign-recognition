04eee9acbc5054aa15fe8fc37bf5eed61e3dd73c
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-15 13:53:05 +0200

    Added a waiter function waiting until we are done

--- detection/PixySerialCommunication/ExtractPixyInfo/ExtractPixyInfo.ino
+++ detection/PixySerialCommunication/ExtractPixyInfo/ExtractPixyInfo.ino
@@ -43,6 +43,7 @@ void loop()
 
 void waitForSignal(){
   int data = 0;
+  Serial.write("Waiting for signal");
   while(true) {
       if(Serial.available() > 0) {
          data = Serial.read();
--- main.py
+++ main.py
@@ -11,11 +11,11 @@ ser = serial.Serial('/dev/ttyACM0', 115200)
 while True:
     detected_img = detect()
     print("Object was detected")
+    ser.write(b'd')
     validated = validate(detected_img)
     print("Object was validated: ", validated)
     if validated:
         new_speed = classifier.classify_single_image(detected_img.filename)
         print("detected sign is: ", new_speed)
-    ser.write(b'd')
         # update_speed(new_speed)
 

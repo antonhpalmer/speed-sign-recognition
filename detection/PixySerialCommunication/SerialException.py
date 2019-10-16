b997fdba02b235341e83b8f51ca3a3825fb0da9e
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-16 14:08:58 +0200

    Updated serial reader to use exceptions to wait until we get the right input

---
+++ detection/PixySerialCommunication/SerialException.py
@@ -0,0 +1,3 @@
+class SerialInputException(Exception):
+    """Raised when the input read from the serial is wrong"""
+    pass
--- detection/PixySerialCommunication/SerialReader.py
+++ detection/PixySerialCommunication/SerialReader.py
@@ -4,7 +4,7 @@
 
 import serial
 import re
-
+from detection.PixySerialCommunication.SerialException import SerialInputException
 from detection.PixySerialCommunication.PixySnapper import get_cropped_image
 
 
@@ -13,22 +13,26 @@ def get_string_from_pattern(pattern, data_string):
     if info:
         return info.group(1)
     else:
-        return "1"
+        raise SerialInputException('We did find the pattern:', pattern, ", in our data string: ", data_string)
+
 
 # TODO: få lavet så den tager en serial ind i stedet.
 def get_serial_data(ser):
-    #print("Ready to read serial: ", serial_number)
-    # We have an error msg on the first line which we ignore.
-    ser.readline()
+    while True:
+        data = ser.readline()
+        data_string = str(data)
+        try:
+            # We just give a regular expression equal to what we are looking for in the data string.
+            x = get_string_from_pattern('x:(.+?),', data_string)
+            y = get_string_from_pattern('y:(.+?),', data_string)
+            w = get_string_from_pattern('w:(.+?),', data_string)
+            h = get_string_from_pattern('h:(.+?),', data_string)
+        except SerialInputException as e:
+            print(e)
+        else:
+            print("x:", x, "y:", y, "w:", w, "h:", h)
+            return get_cropped_image(int(x), int(y), int(w), int(h))
 
-    data = ser.readline()
-    data_string = str(data)
 
-    # We just give a regular expression equal to what we are looking for in the data string.
-    x = get_string_from_pattern('x:(.+?),', data_string)
-    y = get_string_from_pattern('y:(.+?),', data_string)
-    w = get_string_from_pattern('w:(.+?),', data_string)
-    h = get_string_from_pattern('h:(.+?),', data_string)
-
-    print("x:", x, "y:", y, "w:", w, "h:", h)
-    return get_cropped_image(int(x), int(y), int(w), int(h))
+ser1 = serial.Serial('COM4', 115200)
+get_serial_data(ser1)

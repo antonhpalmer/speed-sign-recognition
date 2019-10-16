fe4eb093db8a53f444c8f9715d0e1d4fd2f387d1
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-16 14:14:42 +0200

    Changed the output of get serial data and the changed detect

--- detection/detect.py
+++ detection/detect.py
@@ -1,5 +1,10 @@
 from detection.PixySerialCommunication.SerialReader import get_serial_data
+from detection.PixySerialCommunication.PixySnapper import get_cropped_image
+import serial
+
 
 def detect(ser):
-    path = get_serial_data(ser)
-    return path
+    x, y, w, h = get_serial_data(ser)
+    print("x:", x, "y:", y, "w:", w, "h:", h)
+    img = get_cropped_image(int(x), int(y), int(w), int(h))
+    return img

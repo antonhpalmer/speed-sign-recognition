740e9dd43cd6a86b595dd1306011f777ea232c12
Anton <apalme17@student.aau.dk>
2019-10-18 10:11:12 +0200

    changed model8

--- classification/models/models.py
+++ classification/models/models.py
@@ -156,24 +156,19 @@ def get_model7():
 def get_model8():
     model = Sequential()
 
-    model.add(Conv2D(32, (3, 3), padding='same',
+    model.add(Conv2D(64, (3, 3), padding='same',
                      input_shape=(3, definitions.IMG_SIZE, definitions.IMG_SIZE),
                      activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 
-    model.add(Conv2D(128, (3, 3), padding='same',
+    model.add(Conv2D(256, (3, 3), padding='same',
                      input_shape=(3, definitions.IMG_SIZE, definitions.IMG_SIZE),
                      activation='relu'))
-    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.2))
-
-    model.add(Conv2D(512, (3, 3), padding='same',
-                     input_shape=(3, definitions.IMG_SIZE, definitions.IMG_SIZE),
-                     activation='relu'))
-    model.add(Conv2D(1024, (3, 3), activation='relu'))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
 

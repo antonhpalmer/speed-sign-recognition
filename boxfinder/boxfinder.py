32505458699d0ad8c59d6604ed6af7509a03c433
faje17 <34160668+faje17@users.noreply.github.com>
2019-10-21 02:03:12 +0200

    actually works, pogchamp

--- boxfinder/boxfinder.py
+++ boxfinder/boxfinder.py
@@ -1,74 +1,106 @@
 from PIL import Image
+from PIL import ImageEnhance
 
 #READ! : Just call 'return_coordinates' with appropriate input and it does the job.
 
 #returns True if pixel is red, False if pixel is not red.
 def is_pixel_red (r, g, b):
-    if (r >= 200 & (g + b) <= 150):
-        return True
-    elif (r >= 150 & (g + b) <= 100):
+    if (r > 30 and 10 > g and 10 > b):
         return True
     else:
         return False
 
-#returns x, y coordinates
-def top_left (pix, x, y, loop):
-    for i in range(loop):
+def red_right (pix, x, y, loop_range):
+    for i in range(loop_range):
         (r, g, b) = pix[x, y]
-    
         if (is_pixel_red(r, g, b) == True):
-            return x, y
+            return x, y 
         x += 1
-        y += 1
 
-#returns x, y coordinates
-def top_right (pix, x, y, loop):
-    for i in range(loop):
+def red_left (pix, x, y):
+    for i in range(x-1):
         (r, g, b) = pix[x, y]
-    
         if (is_pixel_red(r, g, b) == True):
             return x, y
-        x += 1
-        y -= 1    
+        x -= 1    
 
-#returns x, y coordinates
-def bottom_left (pix, x, y, loop):
-    for i in range(loop):
+def red_up (pix, x, y):
+    for i in range(y-1):
         (r, g, b) = pix[x, y]
-    
         if (is_pixel_red(r, g, b) == True):
             return x, y
-        x -= 1
-        y += 1
-
-#returns x, y coordinates
-def bottom_right (pix, x, y, loop):
-    for i in range(loop):
-        (r, g, b) = pix[x, y]
-    
-        if (is_pixel_red(r, g, b) == True):
-            return x, y
-        x -= 1
         y -= 1
 
-def loop_range (center_x, center_y, width, height):
-    if center_x >= center_y:
-        return height - center_y 
-    elif center_y > center_x:
-        return width - center_x
+def red_bottom (pix, x, y, loop_range):
+    for i in range(loop_range):
+        (r, g, b) = pix[x, y]    
+        if (is_pixel_red(r, g, b) == True):
+            return x, y
+        y += 1
 
+def distance_to_right_edge (center_x, width):
+    return width - center_x
 
+def distance_to_bottom_edge (center_y, height):
+    return height - center_y
+
+def find_box(pix, x, y, width, height):
+    right_edge_distance = distance_to_right_edge (x, width)
+    bottom_edge_distance = distance_to_bottom_edge (y, height)
+
+    (right_red_x, right_red_y) = red_right(pix, x, y, right_edge_distance)
+    (left_red_x, left_red_y) = red_left(pix, x, y)
+    (up_red_x, up_red_y) = red_up(pix, x, y)
+    (bottom_red_x, bottom_red_y) = red_bottom(pix, x, y, bottom_edge_distance)
+
+    (start_x, start_y) = (left_red_x, up_red_y)
+    x_origin = start_x
+
+    (top_most_x, top_most_y) = (x, y)
+    (bottom_most_x, bottom_most_y) = (x, y)
+    (right_most_x, right_most_y) = (x, y)
+    (left_most_x, left_most_y) = (x, y)
+
+    for i in range(bottom_red_y - up_red_y):
+        for k in range(right_red_x - left_red_x):
+            if (pix[start_x, start_y] == (0, 0, 0)):
+                if (start_y < top_most_y):
+                    (top_most_x, top_most_y) = (start_x, start_y)
+                    continue
+
+                if (start_y > bottom_most_y):
+                    (bottom_most_x, bottom_most_y) = (start_x, start_y)
+                    continue
+
+                if (start_x > right_most_x):
+                    (right_most_x, right_most_y) = (start_x, start_y)
+                    continue
+
+                if (start_x < left_most_x):
+                    (left_most_x, left_most_y) = (start_x, start_y)
+                    continue
+                
+            start_x += 1
+        start_x = x_origin
+        start_y += 1
+            
+    top_left_corner = (left_most_x, top_most_y)
+    bottom_left_corner = (left_most_x, bottom_most_y)
+    top_right_corner = (right_most_x, top_most_y)
+    bottom_right_corner = (right_most_x, bottom_most_y)
+
+    print (top_left_corner, bottom_left_corner, top_right_corner, bottom_right_corner)
+
+    return left_most_x, top_most_y, right_most_x, bottom_most_y
+
+            
 def return_coordinates(image, center_coordinate):
     im = Image.open(image)
     pix = im.load()
-    
     (width, height) = im.size
     (x, y) = center_coordinate
-    
-    loop = loop_range(x, y, width, height)
-    corner_top_left = top_left(pix, x, y, loop)
-    corner_top_right = top_right(pix, x, y, loop)
-    corner_bottom_left = bottom_left(pix, x, y, loop)
-    corner_bottom_right = bottom_right(pix, x, y, loop)
-    
-    return corner_top_left, corner_top_right, corner_bottom_left, corner_bottom_right
+
+    (top_left_x, top_left_y, bottom_right_x, bottom_right_y) = find_box(pix, x, y, width, height)
+    im1 = im.crop((top_left_x, top_left_y, bottom_right_x + 1, bottom_right_y + 1))
+    im1.show()
+        
--- boxfinder/main.py
+++ boxfinder/main.py
@@ -2,5 +2,4 @@ from PIL import Image
 
 from boxfinder import return_coordinates
 
-(x, y, z, w) = return_coordinates ('C:/Users/frede/OneDrive/Skrivebord/circle.jpg', (16, 16))
-print (x, y, z, w)
+return_coordinates ('C:/Users/frede/OneDrive/Dokumenter/GitHub/speed-sign-recognition/boxfinder/test3.ppm', (63, 79))    

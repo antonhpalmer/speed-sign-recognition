362d2aec21da44c5838ae05fbb3733ee573efc4c
EsbenNedergaard <eneder17@student.aau.dk>
2019-10-29 13:52:28 +0100

    Making a test data file transformer

--- test_data/image_transformer/csv_image_transformer.py
+++ test_data/image_transformer/csv_image_transformer.py
@@ -1,6 +1,6 @@
 import csv
 from path_transformer.output_path_creator import create_output_path
-
+from test_data.image_transformer.brigthness_transformer import transform_brightness
 
 def transform_all_images_in_csv_file(csv_input_file_path):
     with open(csv_input_file_path, mode='r+') as input_file:
@@ -13,7 +13,8 @@ def transform_all_images_in_csv_file(csv_input_file_path):
                     # We just skip the first line containing the column names
                     line_count += 1
                 else:
-                    print(f'{row[0]} is a sign of type {row[1]}.')
+                    transform_brightness("test_images/" + row[0])
+                    # print(f'{row[0]} is a sign of type {row[1]}.')
                     line_count += 1
             print('We read a total of:', line_count, "lines")
             print(output_csv_path)

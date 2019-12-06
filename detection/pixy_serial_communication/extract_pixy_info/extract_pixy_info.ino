60c134286c0457131ca151fd73442486d8f767df
EsbenNedergaard <eneder17@student.aau.dk>
2019-12-06 10:38:50 +0100

    Removed free memory from Arduino code

--- detection/pixy_serial_communication/extract_pixy_info/extract_pixy_info.ino
+++ detection/pixy_serial_communication/extract_pixy_info/extract_pixy_info.ino
@@ -18,27 +18,7 @@ void setup()
   evshield.bank_b.motorReset();
 }
 
-extern void *__bss_end;
-extern void *__brkval;
-
-int get_free_memory()
-{
-  int free_memory;
-
-  if((int)__brkval == 0)
-    free_memory = ((int)&free_memory) - ((int)&__bss_end);
-  else
-    free_memory = ((int)&free_memory) - ((int)__brkval);
-
-  return free_memory;
-}
-
-
-void loop()
-{ 
-  /* Serial.print("Free memory:");
-  Serial.print(get_free_memory());
-  Serial.print("\n"); */
+void loop(){
   // grab blocks!
   pixy.ccc.getBlocks();
   int ageNeeded = 25;
